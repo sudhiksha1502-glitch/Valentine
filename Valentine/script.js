db9d63c793f6502ab10937c3c29e1419242e4d2e
sudhiksha1502-glitch <sudhiksha1502@gmail.com>
2026-02-05 09:41:13 +0530

    Change password in login function
    Updated the password in the login function.

--- Valentine/script.js
+++ Valentine/script.js
@@ -2,9 +2,10 @@ function login() {
   const name = document.getElementById("name").value.toLowerCase();
   const pass = document.getElementById("pass").value;
 
-  if (name === "shi shii" && pass === "30112024") {
+  if (name === "shi shii" && pass === "30112023") {
     window.location.href = "gallery.html";
   } else {
     alert("Only my favourite person ❤️");
   }
 }
+
