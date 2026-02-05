b41ed1fabbdd82494fbcc4b55fc721882b3d9591
sudhiksha1502-glitch <sudhiksha1502@gmail.com>
2026-02-05 09:34:41 +0530

    Add files via upload

---
+++ Valentine/script.js
@@ -0,0 +1,10 @@
+function login() {
+  const name = document.getElementById("name").value.toLowerCase();
+  const pass = document.getElementById("pass").value;
+
+  if (name === "shi shii" && pass === "30112024") {
+    window.location.href = "gallery.html";
+  } else {
+    alert("Only my favourite person ❤️");
+  }
+}
